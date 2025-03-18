d41a0d5a91f092d9502ce8a8ed945e3b99255293
yunxincc <1342573954@qq.com>
2025-03-18 21:41:40 +0800

    添加了 gemini 的视觉模型，稍微修改了一下 README 文件

--- app/utils.ts
+++ app/utils.ts
@@ -262,6 +262,9 @@ export function isVisionModel(model: string) {
     "gemini-1.5-pro",
     "gemini-1.5-flash",
     "gemini-2.0-flash",
+    "gemini-2.0-flash-thinking-exp-01-21",
+    "gemini-2.0-pro-exp-02-05",
+    "gemini-2.0-flash-exp",
     "gpt-4o",
     "gpt-4o-mini",
   ];
